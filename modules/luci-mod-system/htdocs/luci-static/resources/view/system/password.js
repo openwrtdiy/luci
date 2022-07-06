83224b92714f0a2b0f1953f3a3745dd009797ff6
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-07-06 14:43:26 +0800

    change web luci user to kokang

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/password.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/password.js
@@ -78,7 +78,7 @@ return view.extend({
 				return;
 			}
 
-			return callSetPassword('root', formData.password.pw1).then(function(success) {
+			return callSetPassword('kokang', formData.password.pw1).then(function(success) {
 				if (success)
 					ui.addNotification(null, E('p', _('The system password has been successfully changed.')), 'info');
 				else
