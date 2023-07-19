6bf07ed6c687c9b6594700c9c3a5c47b68a7e91d
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-07-20 03:27:16 +0800

    Revert "stauts/network: add pppoe online users"
    This reverts commit eadff7bef4e08ed0bd2cd0729fba0196a7432136.

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/30_network.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/30_network.js
@@ -1,13 +1,7 @@
 'use strict';
 'require baseclass';
 'require fs';
-'require rpc';
 'require network';
-
-var callOnlineUsers = rpc.declare({
-        object: 'luci',
-        method: 'getOnlineUsers'
-});
 
 function progressbar(value, max, byte) {
 	var vn = parseInt(value) || 0,
@@ -73,8 +67,7 @@ return baseclass.extend({
 			fs.trimmed('/proc/sys/net/netfilter/nf_conntrack_count'),
 			fs.trimmed('/proc/sys/net/netfilter/nf_conntrack_max'),
 			network.getWANNetworks(),
-			network.getWAN6Networks(),
-			L.resolveDefault(callOnlineUsers(), {})
+			network.getWAN6Networks()
 		]);
 	},
 
@@ -82,34 +75,23 @@ return baseclass.extend({
 		var ct_count  = +data[0],
 		    ct_max    = +data[1],
 		    wan_nets  = data[2],
-		    wan6_nets = data[3],
-		    onlineusers = data[4];
+		    wan6_nets = data[3];
 
 		var fields = [
-			_('Active Connections'), ct_max ? ct_count : null,
-			_('Online Users'), onlineusers ? onlineusers.onlineusers : null
+			_('Active Connections'), ct_max ? ct_count : null
 		];
 
 		var ctstatus = E('table', { 'class': 'table' });
 
 		for (var i = 0; i < fields.length; i += 2) {
-			if (fields[i] == _('Online Users')) {
-				ctstatus.appendChild(E('tr', { 'class': 'tr' }, [
-					E('td', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
-					E('td', { 'class': 'td left' }, [
-						(fields[i + 1] != null) ? fields[i + 1] : '?'
-					])
-				]));
-			} else {
-				ctstatus.appendChild(E('tr', { 'class': 'tr' }, [
-					E('td', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
-					E('td', { 'class': 'td left' }, [
-						(fields[i + 1] != null) ? progressbar(fields[i + 1], ct_max) : '?'
-					])
-				]));
-			}
+			ctstatus.appendChild(E('tr', { 'class': 'tr' }, [
+				E('td', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
+				E('td', { 'class': 'td left' }, [
+					(fields[i + 1] != null) ? progressbar(fields[i + 1], ct_max) : '?'
+				])
+			]));
 		}
-		
+
 		var netstatus = E('div', { 'class': 'network-status-table' });
 
 		for (var i = 0; i < wan_nets.length; i++)
