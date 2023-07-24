0395ac5a971c153692b5ad5909143b3f6b3bf687
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-07-25 03:47:36 +0800

    Revert "luci /modules: Add to display the number of online users"
    This reverts commit e6e7ceb3e4d951872a554f4c8d13a7e323d69f4f.

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/30_network.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/30_network.js
@@ -3,11 +3,6 @@
 'require fs';
 'require network';
 
-var callOnlineUsers = rpc.declare({
-        object: 'luci',
-        method: 'getOnlineUsers'
-});
-
 function progressbar(value, max, byte) {
 	var vn = parseInt(value) || 0,
 	    mn = parseInt(max) || 100,
@@ -72,8 +67,7 @@ return baseclass.extend({
 			fs.trimmed('/proc/sys/net/netfilter/nf_conntrack_count'),
 			fs.trimmed('/proc/sys/net/netfilter/nf_conntrack_max'),
 			network.getWANNetworks(),
-			network.getWAN6Networks(),
-			L.resolveDefault(callOnlineUsers(), {})
+			network.getWAN6Networks()
 		]);
 	},
 
@@ -81,12 +75,10 @@ return baseclass.extend({
 		var ct_count  = +data[0],
 		    ct_max    = +data[1],
 		    wan_nets  = data[2],
-		    wan6_nets = data[3],
-		    onlineusers = data[4];
+		    wan6_nets = data[3];
 
 		var fields = [
-			_('Active Connections'), ct_max ? ct_count : null,
-			_('Online Users'), onlineusers ? onlineusers.onlineusers : null
+			_('Active Connections'), ct_max ? ct_count : null
 		];
 
 		var ctstatus = E('table', { 'class': 'table' });
