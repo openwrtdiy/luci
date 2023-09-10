995394e7d7a4fadaa977ec73c98b470714517eab
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2023-09-11 02:30:46 +0800

    Revert "Increase the date and time when compiling the OpenWrt firmware build is completed"
    This reverts commit f27a415b3dd65aa931cd7f2f31533029ec4ad7e8.

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
@@ -50,14 +50,6 @@ return baseclass.extend({
 				date.getUTCSeconds()
 			);
 		}
-		
-		// Convert the Unix timestamp to a human-readable date and time format
-		var compileTime = null;
-		if (L.isObject(boardinfo.release) && boardinfo.release.compile) {
-		    var compileTimestamp = parseInt(boardinfo.release.compile, 10);
-		    var compileDate = new Date(compileTimestamp * 1000); // Convert seconds to milliseconds
-		    compileTime = compileDate.toLocaleString(); // Format as a human-readable date and time
-		}
 
 		var fields = [
 			_('Hostname'),         boardinfo.hostname,
@@ -66,7 +58,7 @@ return baseclass.extend({
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
 			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
 			_('Kernel Version'),   boardinfo.kernel,
-			_('Compile Time'),     compileTime, // Display the modified compileTime variable
+			_('Compile Time'),     (L.isObject(boardinfo.release) ? boardinfo.release.compile : ''),
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
 			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
